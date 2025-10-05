2b6cc3c005ade81268dbf89d043ace343f3ce344
Chronos Vault <holycyclop1@icloud.com>
2025-10-05 16:46:10 +0300

    Update contracts/ton/wrappers/ChronosVault.compile.ts

---
+++ contracts/ton/wrappers/ChronosVault.compile.ts
@@ -0,0 +1,9 @@
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+    lang: 'func',
+    targets: [
+        'contracts/ton/imports/stdlib.fc',
+        'contracts/ton/ChronosVault.fc'
+    ],
+};
