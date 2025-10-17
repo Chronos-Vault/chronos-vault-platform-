b3df29d136bffe9725859d49964d016c16d35544
imanolaguayo-lgtm <imanolaguayo@gmail.com>
2025-10-17 11:03:32 +0200

    Delete contracts/ton directory

--- contracts/ton/wrappers/CVTBridge.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: [
-        'contracts/ton/imports/stdlib.fc',
-        'contracts/ton/CVTBridge.fc'
-    ],
-};
